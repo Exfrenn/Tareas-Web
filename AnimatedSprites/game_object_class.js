809ea6ef56490f8ab34096a16fb3ed408a52acbe
Efren Chavez <efrenchv05@gmail.com>
2025-04-23 20:26:27 -0600

    Uploading Animated Sprites activity

---
+++ AnimatedSprites/game_object_class.js
@@ -0,0 +1,62 @@
+class GameObject {
+    constructor(position, width, height, color, type) {
+        this.position = position;
+        this.width = width;
+        this.height = height;
+        this.color = color;
+        this.type = type;
+    }
+
+    draw(ctx) {
+        if (this.spriteImage){
+            if(this.spriteRect){
+                ctx.drawImage(this.spriteImage,
+                    this.spriteRect.x, this.spriteRect.y,
+                    this.spriteRect.width, this.spriteRect.height,
+                    this.position.x, this.position.y,
+                    this.width, this.height
+                )
+            }else{
+            ctx.drawImage(this.spriteImage,
+                this.position.x, this.position.y,
+                this.width, this.height
+            )
+        }}else{
+            ctx.fillStyle = this.color;
+            ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
+        }
+    
+        this.drawBoundingBox(ctx);
+    
+    }
+
+
+    drawBoundingBox(ctx) {
+        // Draw the bounding box of the sprite
+        ctx.strokeStyle = "red";
+        ctx.beginPath();
+        ctx.rect(this.position.x, this.position.y,
+                 this.width, this.height);
+        ctx.stroke();
+    }
+
+    update(deltaTime) {
+
+    }
+
+    setSprite(ImagePath,rect) {
+        this.spriteImage = new Image();
+        this.spriteImage.src = ImagePath;
+        if(rect){
+            this.spriteRect = rect;
+        }
+    }
+
+}
+
+function boxOverlap(rect1, rect2) {
+    return rect1.position.x < rect2.position.x + rect2.width &&
+           rect1.position.x + rect1.width > rect2.position.x &&
+           rect1.position.y < rect2.position.y + rect2.height &&
+           rect1.position.y + rect1.height > rect2.position.y;
+}
